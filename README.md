b457ec5779ce815362aad5b5ab7761640ab4aa37
CTW100 <85342844+CTW100@users.noreply.github.com>
2022-01-11 00:30:49 +0900

    #8.7 Introduction to Styled Components

--- README.md
+++ README.md
@@ -16,7 +16,7 @@
 
 # 8.1
 
-npm i @apollo/client @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome graphql react-helmet-async react-hook-form react-router-dom styled-components
+npm i @apollo/client @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome graphql react-helmet-async react-hook-form react-router-dom@5.3.0 styled-components
 
 # 8.3
 
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,39 @@
+import { useState } from 'react';
+import styled, { css } from 'styled-components';
 import { isLoggedInVar } from '../apollo';
 
+// 이것들은 각각 하나의 component이기 때문에 props도 받을 수 있음.
+// css의 field는 그대로 가져가고 내용물만 바꿀 수도 있고, 한편으로는 {css} 를 사용하여 상태에 따라서 완전히 다른 css field를 적용할 수도 있음
+const Title = styled.h1`
+  color: ${(props) => (props.potato ? 'palevioletred' : 'beige')};
+  font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  ${(props) =>
+    props.potato
+      ? css`
+          font-size: 49px;
+        `
+      : css`
+          text-decoration: underline;
+        `}
+`;
+
+const Container = styled.div`
+  background-color: tomato;
+`;
+
+const TogglePotato = styled.button`
+  color: red;
+`;
+
 function Login() {
+  const [potato, setPotato] = useState(false);
+  const TogglePotato = () => setPotato((current) => !current);
   return (
-    <div>
-      <h1>Login</h1>;
-      <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
-    </div>
+    <Container>
+      <Title potato={potato}>Login</Title>;
+      <TogglePotato onClick={TogglePotato}>Toggle Potato</TogglePotato>
+    </Container>
   );
 }
 
