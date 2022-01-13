068dc6b7e28a42d7f920572790989cb6e9310203
CTW100 <85342844+CTW100@users.noreply.github.com>
2022-01-13 16:40:13 +0900

    #11.2 Header part Three

--- src/apollo.js
+++ src/apollo.js
@@ -1,5 +1,11 @@
 // 이 파일은 reactive variables를 가지고 있을 곳
-import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const TOKEN = 'TOKEN';
 const DARK_MODE = 'DARK_MODE';
@@ -28,7 +34,25 @@ export const disableDarkMode = () => {
   darkModeVar(false);
 };
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql', // 너의 GraphQL application이 어딨는지 알려주는 것
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: localStorage.getItem(TOKEN),
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(), // Cache는 Apollo가 한번 가져온 정보를 기억하게 해서 매번 같은 정보를 가져오지 않도록 하는 것
 });
+
+// link는 Apollo 클라이언트가 데이터 소스와 소통하는 방식이야
+// 그러기 위해서는 authLink를 만들어야 됨. 만드는 건 쉬움. 그냥 LocalStorage에서 token을 가져와서 그 token을 authorization의 헤더나 넣어주고 싶은 다른 헤더에 넣어주면 됨
+// setContext는 클라이언트의 모든 request에 몇 가지 항목을 추가하는 gkatndla
+// concat : 두 개의 링크를 묶음
--- src/hook/useUser.js
+++ src/hook/useUser.js
@@ -1,5 +1,6 @@
 import { gql, useQuery, useReactiveVar } from '@apollo/client';
-import { isLoggedInVar } from '../apollo';
+import { useEffect } from 'react';
+import { isLoggedInVar, logUserOut } from '../apollo';
 
 const ME_QUERY = gql`
   query me {
@@ -11,12 +12,18 @@ const ME_QUERY = gql`
 `;
 
 function useUser() {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
-  const { data, error } = useQuery(ME_QUERY, {
+  const hasToken = useReactiveVar(isLoggedInVar);
+  const { data } = useQuery(ME_QUERY, {
     // 사용자가 LocalStorage의 토큰을 통해 로그인하지 않은 경우에는(Local Storage에 토큰을 가지고 있지 않은 경우) 이 쿼리를 실행하지 않음(skip해버림.)
-    skip: !isLoggedIn,
+    skip: !hasToken,
   });
-  console.log(data, error);
+  console.log(data);
+  useEffect(() => {
+    // useEffect는 hook이 마운트되면 한 번 실행되고 데이터가 변경될 때 마다 실행됨
+    if (data?.me === null) {
+      logUserOut(); // token 조작하면 로그아웃시킴
+    }
+  }, [data]);
   return;
 }
 
