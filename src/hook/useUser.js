d3368d74840178969d14c30e1ce89641a1402b2e
CTW100 <85342844+CTW100@users.noreply.github.com>
2022-01-14 14:54:04 +0900

    #11.7 Liking Photos

--- src/hook/useUser.js
+++ src/hook/useUser.js
@@ -18,7 +18,6 @@ function useUser() {
     // 사용자가 LocalStorage의 토큰을 통해 로그인하지 않은 경우에는(Local Storage에 토큰을 가지고 있지 않은 경우) 이 쿼리를 실행하지 않음(skip해버림.)
     skip: !hasToken,
   });
-  console.log(data);
   useEffect(() => {
     // useEffect는 hook이 마운트되면 한 번 실행되고 데이터가 변경될 때 마다 실행됨
     if (data?.me === null) {
